24da9901c3d2d4db939e43a2bd0b2e4b0372c70f
Ravi Varma Injeti <rvarmainjeti@gmail.com>
2021-02-26 17:30:58 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,10 +4,11 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 
-DATABASE_URL = os.environ.get("postgresql://postgres:qwerty@localhost:5432/course_website")
+#SQLALCHEMY_DATABASE_URL = os.environ.get("postgresql://postgres:qwerty@localhost:5432/course_website")
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
 
 engine = create_engine(
-    DATABASE_URL
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
