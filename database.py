50ff38ecea92532147647f0470f6f33495d238d7
Ravi Varma Injeti <rvarmainjeti@gmail.com>
2021-02-26 16:16:33 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:qwerty@localhost:5432/course_website"
+
+DATABASE_URL = os.environ.get("postgresql://postgres:qwerty@localhost:5432/course_website")
 
 engine = create_engine(
     DATABASE_URL
