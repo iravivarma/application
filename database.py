3cdccfb833db501f37debe7e7e551560b9643f31
Ravi Varma Injeti <rvarmainjeti@gmail.com>
2021-02-26 17:34:20 +0530

    Update database.py

--- database.py
+++ database.py
@@ -6,6 +6,7 @@ import os
 
 #SQLALCHEMY_DATABASE_URL = os.environ.get("postgresql://postgres:qwerty@localhost:5432/course_website")
 SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+print(SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
